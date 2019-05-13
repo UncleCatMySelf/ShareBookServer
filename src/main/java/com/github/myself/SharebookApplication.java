9cbea8e26c1ae9db78500f454b7e46150360ff95
朱培杰 <1341933031@qq.com>
2019-05-13 13:42:10 +0800

    初始化

---
+++ src/main/java/com/github/myself/SharebookApplication.java
@@ -0,0 +1,13 @@
+package com.github.myself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharebookApplication.class, args);
+	}
+
+}
